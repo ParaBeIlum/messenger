04211bd710f237f399ab4d9ce817e9012ccb385c
Dmitriy Golikov <43889596+ParaBeIlum@users.noreply.github.com>
2021-10-28 15:13:43 +0300

    Add files via upload
    initial commit

---
+++ client.py
@@ -0,0 +1,46 @@
+import socket
+import errno
+import sys
+import json
+
+SERVER_IP = "192.168.56.1"
+SERVER_PORTS = [8000, 8001]
+
+
+def connect_server(port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((SERVER_IP, port))
+    return client_socket
+
+
+while True:
+    identifier = input("Enter your unique identifier: ")
+    code_getting_socket = connect_server(SERVER_PORTS[0])
+    data = identifier.encode()
+    code_getting_socket.send(data)
+    server_response = code_getting_socket.recv(1024)
+    if server_response:
+        print(server_response.decode())
+        break
+    else:
+        print("Choose another identifier. This one is taken!")
+code_getting_socket.close()
+identifier = input("Enter your identifier: ")
+code = input("Enter your verification code: ")
+message = input("Enter your message: ")
+data = json.dumps({"identifier": identifier, "code": code, "message": message})
+message_sending_socket = connect_server(SERVER_PORTS[1])
+message_sending_socket.send(data.encode())
+try:
+    response = message_sending_socket.recv(1024)
+    if not len(response):
+        print("Connection closed by the server.")
+        sys.exit()
+    print(response.decode())
+except IOError as e:
+    if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+        print(f"Reading error: {str(e)}.")
+        sys.exit()
+except Exception as e:
+    print(f"Reading error: {str(e)}.")
+    sys.exit()
---
+++ server.py
@@ -0,0 +1,131 @@
+import socket
+import select
+import logging
+import pathlib
+import json
+import os
+import uuid
+from datetime import datetime
+
+MAX_CONNECTIONS = 51
+SERVER_IP = "192.168.56.1"
+SERVER_PORTS = [8000, 8001]
+inputs = []
+servers = []
+clients = {}
+
+
+def initialize_logger():
+    """
+    Initializes a logger and generates a log file in ./logs.
+    Returns
+    -------
+    logging.Logger
+        Used for writing logs of varying levels to the console and log file.
+    -------
+    """
+    path = pathlib.Path(os.path.join(os.getcwd(), "logs"))
+    path.mkdir(parents=True, exist_ok=True)
+
+    logger = logging.getLogger('Server')
+    logger.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler()
+    fh = logging.FileHandler(
+        filename=f'logs/{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}_server.log'
+    )
+    ch.setLevel(logging.INFO)
+    fh.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '[%(asctime)s] - %(levelname)s - %(message)s'
+    )
+
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+
+    return logger
+
+
+def generate_id(string):
+    """
+    Generates uuid
+    """
+    return uuid.uuid5(uuid.NAMESPACE_DNS, string)
+
+
+def get_server_socket(port):
+    """
+    Starts the socket server
+
+    Parameters
+    ----------
+    port : int
+        server socket port
+    Returns
+    -------
+    server : object
+        socket server
+    -------
+    """
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((SERVER_IP, port))
+    server.listen(MAX_CONNECTIONS)
+    return server
+
+
+def handle_sockets():
+    """
+    Handles server sockets behavior
+    """
+    readable, _, exceptional = select.select(inputs, [], inputs)
+    for sock in readable:
+        if sock in servers:
+            client_socket, client_address = sock.accept()
+            logger.info(f"New connection from {client_address}.")
+            client_socket.setblocking(0)
+            data = client_socket.recv(1024).decode()
+            if not data:
+                continue
+            if sock == servers[0]:
+                if data in clients:
+                    client_socket.send(''.encode())
+                else:
+                    uid = str(generate_id(data))
+                    encoded = uid.encode()
+                    client_socket.send(encoded)
+                    clients[data] = uid
+                    logger.info(f"Registered new user with identifier <{data}>")
+            else:
+                data_json = json.loads(data)
+                identifier = data_json['identifier']
+                code = data_json['code']
+                mess = data_json['message']
+                if identifier in clients and clients[identifier] == code:
+                    message = "Your message saved to log."
+                    client_socket.send(message.encode())
+                    logger.info(f"MESSAGE - {identifier} >>> {mess}")
+                else:
+                    message = "Error detected. Check your identifier and code."
+                    client_socket.send(message.encode())
+                    logger.error("Wrong identifier - code pair.")
+        continue
+
+
+if __name__ == '__main__':
+    logger = initialize_logger()
+    for port in SERVER_PORTS:
+        server_socket = get_server_socket(port)
+        inputs.append(server_socket)
+        servers.append(server_socket)
+        logger.info(f"Server started on {SERVER_IP}:{port}")
+    try:
+        while True:
+            handle_sockets()
+    except Exception as e:
+        logger.error(e)
+    except KeyboardInterrupt:
+        logger.warning("Keyboard interrupt detected!")
